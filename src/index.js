b2b5e35c6c1eacb99779886f6887d2680d61a96a
KhailoAndrey <hap512@gmail.com>
2023-04-26 12:02:54 +0300

    start

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ const theme = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}></PersistGate>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
+    </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
